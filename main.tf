a32c3780fee3e5ad5472dbff2a5b53e1f3586dd2
DarshanLk <108049596+Drahanlk@users.noreply.github.com>
2022-08-27 17:48:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 	region = "ap-southeast-1"
 }
 resource "aws_instance" "simpleec2" {
-  ami           = "ami-02248c9d6ba0b9e12"
+  ami           = "ami-06ecd61e4bded3bfe"
   instance_type = "t2.micro"
 
   tags = {
@@ -10,6 +10,5 @@ resource "aws_instance" "simpleec2" {
   }
 }
 
-}
 
-}
+
