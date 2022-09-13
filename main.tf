cc16a7bd51ca27fff11c6a5ded8c18c02a3ccdc4
DarshanLk <108049596+Drahanlk@users.noreply.github.com>
2022-09-14 00:33:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ resource "aws_instance" "test1" {
   ami           = "ami-06ecd61e4bded3bfe"
   instance_type = "t2.micro"
 	key_name = "Terraform-singapore"
-	map_public_ip_on_launch = "true"
 	public_ip = "aws_instance.test1.ip"
 
   tags = {
